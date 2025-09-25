8f7d9b342c8fc43e5e6b123433eb2b985acfd64b
Steve8695 <51990522+Steve8695@users.noreply.github.com>
2025-09-25 12:48:28 -0400

    Update app.py

--- service/app.py
+++ service/app.py
@@ -1,4 +1,7 @@
-# WRONG
+# service/app.py
+def add(a, b):
+    return a + b
+
 if __name__ == "__main__":
-    def add(a, b):
-        return a + b
+    # optional test/demo code here
+    print(add(1, 2))
