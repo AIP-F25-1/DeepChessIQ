0563cfd4b6970fc461eee8bfa9665276262c90e8
Steve8695 <51990522+Steve8695@users.noreply.github.com>
2025-09-25 13:35:08 -0400

    Update test_app.py

--- service/test_app.py
+++ service/test_app.py
@@ -2,7 +2,7 @@ import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
 
-import app
+import test_app
 
 def test_basic_route():
     assert True  # Replace with real test if needed
